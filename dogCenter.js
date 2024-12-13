803d0777cf905b764bca50dfad777b730127a72e
lindsey14233 <114265575+lindsey14233@users.noreply.github.com>
2024-12-13 14:43:16 -0500

    removed commented code in dogCenter

--- dogCenter.js
+++ dogCenter.js
@@ -34,13 +34,11 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
 
 /* MongoDB functions */
-/* For the Application pages */
 async function insertApp(name, email, age, addInfo) {
     try {
         await client.connect();
        
         /* Inserting one app */
-        // console.log("***** Inserting one app *****");
         let newApp = {name: name, email: email, age: age, addInfo: addInfo, dogs: []};
         await insertAppHelper(client, databaseAndCollection, newApp);
 
@@ -64,29 +62,13 @@ async function lookUpOneEntry(username) {
     return result;
 }
 
-/* For the select GPA pages */
-// async function lookUpMany(givenGpa) {
-//     let filter = {gpa : { $gte: givenGpa}}; // >= (gpa: givenGpa)
-//     const cursor = client.db(databaseAndCollection.db)
-//     .collection(databaseAndCollection.collection)
-//     .find(filter);
-
-//     // Some Additional comparison query operators: $eq, $gt, $lt, $lte, $ne (not equal)
-//     // Full listing at https://www.mongodb.com/docs/manual/reference/operator/query-comparison/
-//     const result = await cursor.toArray();
-//     return result;
-//     // console.log(result);
-// }
-
 /* For the Remove All pages */
 async function removeAll() {
     try {
         await client.connect();
-        // console.log("***** Clearing Collection *****");
         const result = await client.db(databaseAndCollection.db)
         .collection(databaseAndCollection.collection)
         .deleteMany({});
-        // console.log(`Deleted documents ${result.deletedCount}`);
         return result.deletedCount;
     } catch (e) {
         console.error(e);
@@ -99,10 +81,7 @@ async function removeAll() {
 
 /* Definining and Processing Endpoints (path and specific HTTP method) stuff */
 
-/* This endpoint renders the main page of the application 
-	and it will display the contents of the index.ejs template file. */
 app.get("/", (request, response) => { 
-    /* You implement */
     response.render("index");
 });
 
@@ -125,7 +104,6 @@ app.post("/registerSuccess", (request, response) => {
 
 /* The following two endpoints handle the Application pages */
 app.get("/adoption", (request, response) => { 
-	/* You implement */ 
     const variables = {
         postUrl: homeUrl + "/adoptionSuccess",
     };
@@ -157,23 +135,6 @@ app.post("/adoptionSuccess", (request, response) => {
                 results: "Invalid User",
             };
         }
-        /*try {
-            let result = await lookUpOneEntry(reqName);
-
-
-            let {name, email, age, addInfo, dogs} = result;
-    
-            let answer = "<h1>Applicants Data</h1>";
-            answer += "<b>Name: </b>&nbsp" + name + "<br>";
-            answer += "<b>Email: </b>&nbsp" + email + "<br>";
-            answer += "<b>GPA: </b>&nbsp" + age + "<br>";
-            answer += "<b>Background Information: </b><br>" + addInfo + "<br>";
-            answer += "<a href=\"/\">HOME</a>";
-    
-            response.writeHead(statusCode, {"Content-type": "text/html"});
-            response.end(answer);
-        } catch (e) {}
-        let name =  request.body;*/
         response.render("result", variables);
     }
     adopt();
@@ -183,18 +144,11 @@ app.post("/adoptionSuccess", (request, response) => {
 app.get("/backyard", (request, response) => {
     const variables = {
         postUrl: homeUrl + "/yourBackyard",
-        // homeUrl: localUrl,
     };
 
     response.render("backyard", variables);
 });
 app.post("/yourBackyard", (request, response) => { 
-    /* Notice how we are extracting the values from request.body */
-    
-    //let statusCode = 200; // success!
-
-    // console.log("reqEmail is "+reqEmail);
-
     //MongoDB function!!
     async function backyard() {
         await client.connect();
@@ -220,35 +174,6 @@ app.post("/yourBackyard", (request, response) => {
             };
         }
         response.render("result", variables);
-        /*try {
-            let result = await lookUpOneEntry(reqName);
-
-            let {name, email, age, addInfo, dogs} = result;
-    
-            let answer = "<h1>Applicants Data</h1>";
-            answer += "<b>Name: </b>&nbsp" + name + "<br>";
-            answer += "<b>Email: </b>&nbsp" + email + "<br>";
-            answer += "<b>GPA: </b>&nbsp" + age + "<br>";
-            answer += "<b>Background Information: </b><br>" + addInfo + "<br>";
-            answer += "<a href=\"/\">HOME</a>";
-    
-            response.writeHead(statusCode, {"Content-type": "text/html"});
-            response.end(answer);
-        } catch (e) {
-            // console.error(e);
-
-            let answer = "<h1>Applicants Data</h1>";
-            answer += "<b>Name: </b>&nbspNONE<br>";
-            answer += "<b>Email: </b>&nbspNONE<br>";
-            answer += "<b>GPA: </b>&nbspNONE<br>";
-            answer += "<b>Background Information: </b><br>NONE<br>";
-            answer += "<a href=\"/\">HOME</a>";
-    
-            response.writeHead(statusCode, {"Content-type": "text/html"});
-            response.end(answer);
-        } finally {
-            await client.close();
-        }*/
     }
     backyard();
 });
@@ -257,7 +182,6 @@ app.post("/yourBackyard", (request, response) => {
 app.get("/returnDog", (request, response) => {
     const variables = {
         postUrl: homeUrl+ "/processReturnDog",
-        // homeUrl: localUrl,
     };
 
     response.render("returnDog", variables);
@@ -269,7 +193,7 @@ app.post("/processReturnDog", (request, response) => {
   
         const variables = {
             header: "Remove All Users and Their Adopted Dogs",
-            results: "All Applications have been removed from the database. Number of applications removed: " + numberRemoved + "<br><br><br>",
+            results: "All users and dogs have been removed from the database. Number of registered users removed: " + numberRemoved + "<br><br><br>",
         };
         response.render("result", variables);
     }
@@ -279,12 +203,6 @@ app.post("/processReturnDog", (request, response) => {
 /* Important */
 app.listen(portNumber);
 process.stdin.setEncoding("utf8");
-
-/* Command Prompt - stops if incorrect num of arguments */
-// if (process.argv.length != 3) {
-//   process.stdout.write(`Usage supermarketServer.js portNum`);
-//   process.exit(1);
-// }
 
 console.log(`Web server started and running at http://localhost:${portNumber}`);
 
